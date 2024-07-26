8adf75ecbd9d460fd98b64e76d6f440ba2a7fda8
unknown <sarpersirinbk@gmail.com>
2024-07-26 14:03:43 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# dnemee
+# dnemee
---
+++ config.js
@@ -0,0 +1,18 @@
+module.exports = {
+
+    token: "MTI2NTYwMDQ5OTI3MzY5OTMzOQ.GeCPiT.04Y9Nr7sNxJU_KJ2IBb69WvYVCv1IyOoClwnI0", //bot tokeniniz
+    clientId: "1265600499273699339", //Bot idsi
+    key: "K81916576188957", //ocr.space key link : https://ocr.space
+
+    //Alttakileri elleme !
+    langs: [
+        'Subscribed',
+        'Aboneolundu',
+        'Abunəoldu',
+        'Abone olundu',
+        'Abunə oldu',
+        'Gezeichnet',
+        'Abonné',
+        'Εγγεγραμμένος'
+    ],
+}
