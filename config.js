18d38a31da12d307befdbca59d433d8e9a60c687
sarpersirin <101975757+sarpersirin@users.noreply.github.com>
2024-07-26 15:36:18 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-    token: "MTI2NTYwMDQ5OTI3MzY5OTMzOQ.Gt0UqW.f_b6JGWrpjQ-Qpvt4P9U6TuJdnmbPd0LBe8RMM", //bot tokeniniz
+    token: "MTI2NTYwMDQ5OTI3MzY5OTMzOQ.Gf54ts.bQriDHsUuALVyVGBkXsYSMKvqUFYmwFnlzf8Bs", //bot tokeniniz
     clientId: "1265600499273699339", //Bot idsi
     key: "K81916576188957", //ocr.space key link : https://ocr.space
 
